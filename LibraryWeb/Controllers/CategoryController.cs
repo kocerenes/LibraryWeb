21655f2efb8cdb9ebd1ef2247394e289d98e9146
enes koçer <enes.kocer1@ogr.sakarya.edu.tr>
2021-09-06 15:27:43 +0300

    category controller is over.

--- LibraryWeb/Controllers/CategoryController.cs
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -18,11 +18,43 @@ namespace LibraryWeb.Controllers
         }
 
         //Yeni kategori ekleme
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        
+        [HttpPost]
         public ActionResult AddCategory(Categories category)
         {
             libraryEntities.Categories.Add(category); //kategoriyi ekledim
             libraryEntities.SaveChanges(); //değişikleri kaydettim
-            return View();
+            return RedirectToAction("Index");
+        }
+
+        //kategori silme işlemleri
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = libraryEntities.Categories.Find(id);
+            libraryEntities.Categories.Remove(category);
+            libraryEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //kategoriyi getiren metot
+        public ActionResult GetCategory(int id)
+        {
+            var category = libraryEntities.Categories.Find(id);
+            return View("GetCategory", category);
+        }
+
+        //kategoriyi güncelleyen metot
+        public ActionResult UpdateCategory(Categories category)
+        {
+            var cat = libraryEntities.Categories.Find(category.ID);
+            cat.NAME = category.NAME;
+            libraryEntities.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
